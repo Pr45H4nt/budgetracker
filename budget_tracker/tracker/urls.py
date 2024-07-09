7082834fb9a3bff7bf44082b6819eb21bafb3fdd
PR45H4NT <work4prash@gmail.com>
2024-07-09 22:51:37 +0545

    views

--- budget_tracker/tracker/urls.py
+++ budget_tracker/tracker/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from . import views
 from . import forms
+from . import models
 
 urlpatterns = [
     path("", views.indexview, name = "home"),
     path("add/income", views.addview, {'formtype':forms.IncomeForm}, name = "addincome"),
     path("add/incomecategory", views.addview, {'formtype': forms.IncomeCategoryForm}, name = "addincomecategory"),
     path("add/expense", views.addview, {'formtype': forms.ExpenseForm}, name = "addexpense"),
-    path("add/incomecategory", views.addview, {'formtype': forms.ExpenseCategoryForm}, name = "addexpensecategory"),
+    path("add/expensecategory", views.addview, {'formtype': forms.ExpenseCategoryForm}, name = "addexpensecategory"),
+    path('show/income', views.showdb , {'database' : models.Income , 'datacategory': models.IncomeCategory}, name= 'showincome' ),
+    path('show/expense', views.showdb , {'database' : models.Expense , 'datacategory': models.ExpenseCategory}, name= 'showexpense' ),
 ]
--- budget_tracker/tracker/views.py
+++ budget_tracker/tracker/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from .forms import *
 from django.db.models import Sum
+from django.contrib import messages
+from datetime import timedelta
+from django.utils import timezone
+
 
 # Create your views here.
 
 def indexview(request):
     total_income = Income.objects.aggregate(total = Sum('amount'))['total'] or 0
+    total_expense = Expense.objects.aggregate(total = Sum('amount'))['total'] or 0
 
-    context = {'total_income':total_income}
+    context = {'total_income':total_income , 'total_expense': total_expense}
     return render(request,'index.html', context)
 
 
@@ -16,9 +21,29 @@ def addview(request, formtype):
         form = formtype(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(request, "Sucessfully Added")
+        else:
+            messages.error(request, "Something went wrong!!")
+        
 
     else:
         form = formtype()
     context = {'form': form}
     return render(request , 'add.html', context)
+
+def showdb(request , database, datacategory):
+    today = timezone.now().date()
+    seven_days_ago = today - timedelta(days=7)
+
+
+    data = database.objects.filter(date__gte = seven_days_ago)
+    category = datacategory.objects.all()
+
+    context = {
+        'data' : data ,
+        'category' : category
+    }
+
+    return render(request , 'datas.html', context)
+    
 
